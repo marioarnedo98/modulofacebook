f518e54d9275f9205c945fcc8b24a6664d211954
marioarnedo98 <39102985+marioarnedo98@users.noreply.github.com>
2018-08-14 13:42:29 +0200

    Version 1.0

---
+++ mod_esfacebook/helper.php
@@ -0,0 +1,24 @@
+<?php
+
+class modEsfacebookHelper
+{
+    /**
+     * Retrieves the hello message
+     *
+     * @param   array  $params An object containing the module parameters
+     *
+     * @access public
+     */    
+    public static function getId($id){
+
+        return $id;
+
+    }
+    public static function getUrl($url){
+        
+        return $url;
+
+    }
+}
+
+?>
---
+++ mod_esfacebook/tmpl/default.php
@@ -0,0 +1,11 @@
+<?php 
+// No direct access
+defined('_JEXEC') or die; ?>
+
+<div id="fb-root"></div>
+
+<div class="fb-page" data-href="<?php echo $url ;?>" data-tabs="timeline" data-height="625" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true">
+<blockquote cite="<?php echo $url; ?>" class="fb-xfbml-parse-ignore"><a href="<?php echo $url; ?>"></a></blockquote>
+</div>
+
+
